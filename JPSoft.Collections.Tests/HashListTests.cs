c3221986642e32369e489d5c271d6b4b0ef854ba
JMPSequeira <jmpsequeira@gmail.com>
2018-03-12 00:06:59 +0000

    HashList

---
+++ JPSoft.Collections.Tests/HashListTests.cs
@@ -0,0 +1,542 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JPSoft.Collections.Generics;
+using NUnit.Framework;
+
+namespace JPSoft.Collections.Tests
+{
+	public class HashListTests
+	{
+
+		public class Dummy : IComparable<Dummy>
+		{
+			static int count = 0;
+			readonly int index = count++;
+
+			public int CompareTo(Dummy other)
+			{
+				return index.CompareTo(other.index);
+			}
+		}
+
+		HashList<Dummy> dic;
+
+		[SetUp]
+		public void Setup()
+		{
+			dic = new HashList<Dummy>();
+		}
+
+		[Test]
+		public void Add_Valid_Adds()
+		{
+			dic.Add(new Dummy());
+
+			Assert.IsNotEmpty(dic);
+		}
+
+		[Test]
+		public void Add_Valid_CountReflectsAdded()
+		{
+			dic.Add(new Dummy());
+			dic.Add(new Dummy());
+
+			Assert.AreEqual(2, dic.Count);
+		}
+
+		[Test]
+		public void Add_Existing_ThrowsArgumentException()
+		{
+			var same = new Dummy();
+
+			dic.Add(same);
+
+			Assert.Throws<ArgumentException>(() => dic.Add(same));
+		}
+
+		[Test]
+		public void Add_Null_ThrowsArgumentNullException()
+		{
+			Dummy invalid = null;
+			Assert.Throws<ArgumentNullException>(() => dic.Add(invalid));
+		}
+
+		[Test]
+		public void AddRange_Valid_Adds()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+
+			Assert.IsNotEmpty(dic);
+		}
+
+		[Test]
+		public void AddRange_Valid_Appends()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.Add(new Dummy());
+
+			dic.AddRange(items);
+
+			Assert.IsTrue(dic.Skip(1).SequenceEqual(items));
+		}
+
+		[Test]
+		public void AddRange_CollectionWithNull_ThrowsArgumentNullException()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				null
+			};
+
+			Assert.Throws<ArgumentNullException>(() => dic.AddRange(items));
+		}
+
+		[Test]
+		public void AddRange_CollectionWithExisting_ThrowsArgumentException()
+		{
+			var same = new Dummy();
+			dic.Add(same);
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				same
+			};
+
+			Assert.Throws<ArgumentException>(() => dic.AddRange(items));
+		}
+
+		[Test]
+		public void AddRange_Valid_CountReflectsAdded()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+			};
+
+			dic.AddRange(items);
+
+			Assert.AreEqual(2, dic.Count);
+		}
+
+		[Test]
+		public void IndexOf_Existing_CorrectIndex()
+		{
+			var dummyAtOne = new Dummy();
+
+			dic.AddRange(new Dummy[] { dummyAtOne, new Dummy(), new Dummy() });
+
+			Assert.AreEqual(0, dic.IndexOf(dummyAtOne));
+		}
+
+		[Test]
+		public void IndexOf_NonExisting_CorrectIndex()
+		{
+			dic.AddRange(new Dummy[] { new Dummy(), new Dummy() });
+
+			Assert.AreEqual(-1, dic.IndexOf(new Dummy()));
+		}
+
+		[Test]
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(2)]
+		public void Insert_ValidIndex_Inserts(int validIndex)
+		{
+			dic.AddRange(new Dummy[] { new Dummy(), new Dummy(), new Dummy() });
+
+			var dummy = new Dummy();
+
+			dic.Insert(validIndex, dummy);
+
+			Assert.AreEqual(dummy, dic[validIndex]);
+		}
+
+		public void Insert_ValidIndex_CountReflectsInserted(int validIndex)
+		{
+			dic.AddRange(new Dummy[] { new Dummy(), new Dummy(), new Dummy() });
+
+			var dummy = new Dummy();
+
+			dic.Insert(validIndex, dummy);
+
+			Assert.AreEqual(4, dic.Count);
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(3)]
+		public void Insert_InvalidIndex_ThrowsArgumentOutOfRange(int invalidIndex)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() =>
+				dic.Insert(invalidIndex, new Dummy()));
+		}
+
+		[Test]
+		public void Insert_Existing_ThrowsArgumentException()
+		{
+			var dummy = new Dummy();
+
+			dic.Add(dummy);
+
+			Assert.Throws<ArgumentException>(() =>
+				dic.Insert(0, dummy));
+		}
+
+		[Test]
+		public void Insert_Valid_CountReflectsAdded()
+		{
+			dic.Add(new Dummy());
+			dic.Insert(0, new Dummy());
+
+			Assert.AreEqual(2, dic.Count);
+		}
+
+		[Test]
+		public void Insert_Null_ThrowsArgumentNullException()
+		{
+			var dummy = new Dummy();
+
+			dic.Add(dummy);
+
+			Assert.Throws<ArgumentNullException>(() =>
+				dic.Insert(0, null));
+		}
+
+		[Test]
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(2)]
+		public void InsertRange_ValidCollection_Insert(int index)
+		{
+			var first = new Dummy();
+
+			var items = new List<Dummy>
+			{
+				first,
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(new Dummy[] { new Dummy(), new Dummy(), new Dummy() });
+
+			dic.InsertRange(index, items);
+
+			Assert.AreEqual(first, dic[index]);
+		}
+
+		[Test]
+		public void InsertRange_CollectionWithNull_ThrowsArgumentNullException()
+		{
+			dic.Add(new Dummy());
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				null
+			};
+
+			Assert.Throws<ArgumentNullException>(() => dic.InsertRange(0, items));
+		}
+
+		[Test]
+		public void InsertRange_CollectionWithExisting_ThrowsArgumentException()
+		{
+			var same = new Dummy();
+			dic.Add(same);
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				same
+			};
+
+			Assert.Throws<ArgumentException>(() => dic.InsertRange(0, items));
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(3)]
+		public void InsertRange_InvalidIndex_ThrowsArgumentOutOfRange(int invalidIndex)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() =>
+				dic.InsertRange(invalidIndex, new Dummy[] { new Dummy(), new Dummy() }));
+		}
+
+		[Test]
+		public void InsertRange_ValidCollection_CountReflectsAdded()
+		{
+			dic.Add(new Dummy());
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.InsertRange(0, items);
+
+			Assert.AreEqual(3, dic.Count);
+		}
+
+		[Test]
+		public void Remove_Existing_Removes()
+		{
+			var existing = new Dummy();
+
+			dic.Add(existing);
+
+			dic.Remove(existing);
+
+			Assert.IsEmpty(dic);
+		}
+
+		[Test]
+		public void Remove_Existing_True()
+		{
+			var existing = new Dummy();
+
+			dic.Add(existing);
+
+			Assert.IsTrue(dic.Remove(existing));
+		}
+
+		[Test]
+		public void Remove_Existing_CountReflectsRemoved()
+		{
+			var existing = new Dummy();
+
+			dic.Add(existing);
+
+			dic.Add(new Dummy());
+
+			dic.Remove(existing);
+
+			Assert.AreEqual(1, dic.Count);
+		}
+
+		[Test]
+		public void Remove_Existing_False()
+		{
+			Assert.IsFalse(dic.Remove(new Dummy()));
+		}
+
+		[Test]
+		[TestCase(0)]
+		[TestCase(1)]
+		[TestCase(2)]
+		public void RemoveAt_Existing_Removes(int index)
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+				new Dummy()
+			};
+
+			var expected = items.ElementAt(index);
+
+			dic.AddRange(items);
+
+			dic.RemoveAt(index);
+
+			Assert.AreNotEqual(expected, dic[index]);
+		}
+
+		[Test]
+		public void RemoveAt_Existing_CountReflects()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+			};
+
+			dic.AddRange(items);
+
+			dic.RemoveAt(1);
+
+			Assert.AreEqual(3, dic.Count);
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(5)]
+		public void RemoveAt_InvalidIndex_ThrowsArgumentOutOfRangeException(int index)
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+			};
+
+			dic.AddRange(items);
+
+			Assert.Throws<ArgumentOutOfRangeException>(() => dic.RemoveAt(index));
+		}
+
+		[Test]
+		public void RemoveRange_Valid_CountReflects()
+		{
+			dic.Add(new Dummy());
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+
+			dic.RemoveRange(0, 2);
+
+			Assert.AreEqual(1, dic.Count);
+		}
+
+		[Test]
+		public void RemoveRange_Valid_Removes()
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+
+			dic.RemoveRange(0, 2);
+
+			Assert.IsEmpty(dic);
+		}
+
+		[Test]
+		[TestCase(0, 1)]
+		[TestCase(1, 1)]
+		[TestCase(0, 2)]
+		[TestCase(2, 1)]
+		public void RemoveRange_Valid_RemovesCorrect(int index, int count)
+		{
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy(),
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+			items.RemoveRange(index, count);
+			dic.RemoveRange(index, count);
+
+			Assert.IsTrue(items.SequenceEqual(dic));
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(5)]
+		public void RemoveRange_InvalidIndex_ThrowsArgumentOutOfRangeException(int index)
+		{
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+
+			Assert.Throws<ArgumentOutOfRangeException>(() => dic.RemoveRange(index, 1));
+		}
+
+		[Test]
+		public void RemoveRange_OutOfRange_ThrowsArgumentOutOfRangeException()
+		{
+			var count = -1;
+
+			var items = new List<Dummy>
+			{
+				new Dummy(),
+				new Dummy()
+			};
+
+			dic.AddRange(items);
+
+			Assert.Throws<ArgumentOutOfRangeException>(() => dic.RemoveRange(0, count));
+		}
+
+		[Test]
+		public void IndexGet_Valid_Item()
+		{
+			var dummy = new Dummy();
+
+			dic.Add(dummy);
+
+			Assert.AreEqual(dummy, dic[0]);
+		}
+
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(2)]
+		public void IndexGet_Invalid_ThrowsKeyNotFoundException(int index)
+		{
+			var dummy = new Dummy();
+
+			dic.Add(dummy);
+
+			Assert.Throws<ArgumentOutOfRangeException>(() => dic[index].ToString());
+		}
+
+		[Test]
+		public void IndexSet_Valid_Adds()
+		{
+			var dummy = new Dummy();
+
+			dic[0] = dummy;
+
+			Assert.AreEqual(dummy, dic.ElementAt(0));
+		}
+
+		[Test]
+		public void IndexSet_Valid_CountReflects()
+		{
+
+			dic[0] = new Dummy();
+
+			Assert.AreEqual(1, dic.Count);
+		}
+
+		[Test]
+		public void IndexSet_InvalidIndex_ThrowsKeyNotFoundE()
+		{
+
+			dic[0] = new Dummy();
+
+			Assert.AreEqual(1, dic.Count);
+		}
+	}
+}
---
+++ JPSoft.Collections/HashList.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JPSoft.Collections.Generics
+{
+	public class HashList<T> : IList<T> where T : IComparable<T>
+	{
+		protected readonly Dictionary<T, int> _indexes = new Dictionary<T, int>();
+
+		protected readonly Dictionary<int, T> _items = new Dictionary<int, T>();
+
+		protected int InnerCount;
+
+		public int Count => InnerCount;
+
+		public virtual bool IsReadOnly => false;
+
+		public virtual T this[int index]
+		{
+			get
+			{
+				ThrowOnInvalid(index);
+
+				return _items[index];
+			}
+
+			set
+			{
+				if (index < 0 && index > InnerCount)
+					throw new ArgumentOutOfRangeException();
+
+				ThrowOnInvalid(value);
+
+				Include(index, value);
+
+			}
+		}
+
+		public void Clear()
+		{
+			_items.Clear();
+			_indexes.Clear();
+		}
+
+		public bool Contains(T item) => _indexes.ContainsKey(item);
+
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			if (array is null)
+				throw new ArgumentNullException();
+
+			if (array.Rank > 1)
+				throw new ArgumentException("Only one-dimensional array is supported.");
+
+			if (arrayIndex < 0)
+				throw new ArgumentOutOfRangeException("arrayIndex is less than array lower bound.");
+
+			if (array.Length < arrayIndex + InnerCount)
+				throw new ArgumentException("Target array cannot hold source array, starting at index " + arrayIndex);
+
+			for (var i = 0; i < InnerCount; i++)
+				array[i] = _items[i];
+
+		}
+
+		public void Add(T item)
+		{
+			ThrowOnInvalid(item);
+
+			Include(InnerCount, item);
+		}
+
+		public void AddRange(IEnumerable<T> items) =>
+			Include(InnerCount, items);
+
+		public int IndexOf(T item)
+		{
+			if (_indexes.TryGetValue(item, out var index))
+				return index;
+
+			return -1;
+		}
+
+		public void Insert(int index, T item)
+		{
+			ThrowOnInvalid(index);
+
+			ThrowOnInvalid(item);
+
+			var i = InnerCount;
+
+			for (; i > index; i--)
+			{
+				var value = _items[i - 1];
+
+				_indexes[value]++;
+
+				_items[i] = value;
+			}
+
+			Include(index, item);
+		}
+
+		public void InsertRange(int index, IEnumerable<T> items)
+		{
+			ThrowOnInvalid(index);
+
+			Include(index, items);
+		}
+
+		public bool Remove(T item)
+		{
+			if (_indexes.TryGetValue(item, out var index))
+			{
+				var threshold = InnerCount - 1;
+
+				for (; index < threshold; index++)
+				{
+					var value = _items[index + 1];
+
+					_items[index] = value;
+
+					_indexes[value]--;
+				}
+
+				Exclude(index, item);
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public void RemoveAt(int index)
+		{
+			ThrowOnInvalid(index);
+
+			Remove(_items[index]);
+		}
+
+		public void RemoveRange(int index, int count)
+		{
+			ThrowOnInvalid(index);
+
+			if (count < 0)
+				throw new ArgumentOutOfRangeException();
+
+			var lastIndex = index + count;
+
+			if (lastIndex > InnerCount)
+				throw new ArgumentException();
+
+			for (; index < lastIndex; index++)
+			{
+				var item = _items[index];
+
+				_items.Remove(index);
+
+				_indexes.Remove(item);
+			}
+
+			for (; lastIndex < InnerCount; lastIndex++)
+			{
+				var item = _items[lastIndex];
+
+				_items.Remove(lastIndex);
+
+				var current = _indexes[item] -= count;
+
+				_items[current] = item;
+			}
+
+			InnerCount -= count;
+		}
+
+		public IEnumerator<T> GetEnumerator() =>
+		_items.OrderBy(o => o.Key).Select(o => o.Value).GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		protected virtual void Include(int index, T item)
+		{
+			_indexes[item] = index;
+
+			_items[index] = item;
+
+			InnerCount++;
+		}
+
+		protected virtual void Include(int index, IEnumerable<T> items)
+		{
+			var count = GetCountOrThrowOnInvalid(items);
+
+			var i = InnerCount - 1;
+
+			var threshold = index - 1;
+
+			for (; i != threshold; i--)
+			{
+				var value = _items[i];
+
+				_indexes[value] += count;
+
+				_items[i + count] = value;
+			}
+
+			foreach (var item in items)
+				Include(index++, item);
+		}
+
+		protected virtual void Exclude(int index, T item)
+		{
+			_indexes.Remove(item);
+
+			_items.Remove(index);
+
+			InnerCount--;
+		}
+
+		protected virtual int GetCountOrThrowOnInvalid(IEnumerable<T> items)
+		{
+			var count = 0;
+
+			foreach (var item in items)
+			{
+				ThrowOnInvalid(item);
+
+				count++;
+			}
+
+			return count;
+		}
+
+		protected virtual void ThrowOnInvalid(T item)
+		{
+			if (item == null)
+				throw new ArgumentNullException();
+
+			if (_indexes.ContainsKey(item))
+				throw new ArgumentException();
+		}
+
+		protected virtual void ThrowOnInvalid(int index)
+		{
+			if (index < 0 || index >= InnerCount)
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+}
